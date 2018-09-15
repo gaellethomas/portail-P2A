a064bfc8c20cdc76c578497af57a0a07251a9b52
unknown <gaelle.thomas.pf1@gmail.com>
2018-09-16 01:03:20 +0200

    first commit

---
+++ src/main/java/co/simplon/PortailP2AApplication.java
@@ -0,0 +1,12 @@
+package co.simplon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailP2AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailP2AApplication.class, args);
+	}
+}
